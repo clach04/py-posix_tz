5c1ea0b71627231423aa10835a31414cad5490a1
clach04 <clach04@gmail.com>
2025-02-23 14:37:11 -0800

    Start to support parsing offset (no-dst)

--- test_posix_tz.py
+++ test_posix_tz.py
@@ -39,6 +39,16 @@ class UtcOffsets(MyBaseTestCase):
     def test_dstoffset(self):
         assert_is_none(self.parsed.dst_offset)  # TODO revisit this
 
+class IndiaOffsets(MyBaseTestCase):
+    def __init__(self):
+        self.parsed = posix_tz.parse_tz('IST-5:30')
+
+    def test_offset(self):
+        assert_equal(19800, self.parsed.offset)
+
+    def test_dstoffset(self):
+        assert_is_none(self.parsed.dst_offset)  # TODO revisit this
+
 class UsaLosAngelesOffsets(MyBaseTestCase):
     def __init__(self):
         self.parsed = posix_tz.parse_tz('PST8PDT,M3.2.0,M11.1.0')
@@ -65,6 +75,32 @@ class UsaLosAngeles_2am(UsaLosAngeles):
     def __init__(self):
         self.parsed = posix_tz.parse_tz('PST8PDT,M3.2.0/2:00:00,M11.1.0/2:00:00')
 
+class UsaNewYorkOffsets(MyBaseTestCase):
+    def __init__(self):
+        self.parsed = posix_tz.parse_tz('EST5EDT,M3.2.0,M11.1.0')
+
+    def test_offset(self):
+        assert_equal(5 * 60 * 60, self.parsed.offset)
+
+    def test_dstoffset(self):
+        assert_equal(6 * 60 * 60, self.parsed.dst_offset)
+
+class UsaNewYork(MyBaseTestCase):
+    def __init__(self):
+        self.parsed = posix_tz.parse_tz('EST5EDT,M3.2.0,M11.1.0')
+
+    def test_2025(self):
+        start_date = time.localtime(posix_tz.determine_change(self.parsed.start, 2025))
+        end_date = time.localtime(posix_tz.determine_change(self.parsed.end, 2025))
+        canon_start_date = (2025, 3, 9, 2, 0, 0, 6, 68)
+        canon_end_date = (2025, 11, 2, 2, 0, 0, 6, 306)
+        assert_equal(canon_end_date, end_date)
+        assert_equal(canon_start_date, start_date)
+
+class UsaNewYork_2am(UsaLosAngeles):
+    def __init__(self):
+        self.parsed = posix_tz.parse_tz('EST5EDT,M3.2.0/2:00:00,M11.1.0/2:00:00')
+
 class UsaLosAngeles_2am_end_only(UsaLosAngeles):
     def __init__(self):
         self.parsed = posix_tz.parse_tz('PST8PDT,M3.2.0,M11.1.0/2:00:00')
@@ -85,6 +121,10 @@ for test_class in (  # FIXME, automate this
     UsaLosAngeles,
     UsaLosAngeles_2am,
     #UsaLosAngeles_2am_end_only,  # TODO implement
+    IndiaOffsets,
+    #UsaNewYorkOffsets,
+    #UsaNewYork,
+    #UsaNewYork_2am,
     ):
         x = test_class()
         x.run()
